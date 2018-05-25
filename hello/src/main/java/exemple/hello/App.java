a134d050bb4dfef2277d699eb58f6cfe3ce32e2d
gsouza <gsouza@CIWRKST06E0C04.cipucrs.pucrs.br>
2018-05-25 15:35:12 -0300

    Teste 01

---
+++ hello/src/main/java/exemple/hello/App.java
@@ -0,0 +1,22 @@
+package exemple.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	getGreeting();
+    }
+
+	
+
+	/**
+	 * 
+	 */
+	public static String getGreeting() {
+		return "Hello, World!";
+	}
+}
